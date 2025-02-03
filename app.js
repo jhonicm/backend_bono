4d15a75b09da0e82d8c4c79ff12f956f888ad250
Jhon Cordova <jcordovam8@est.ups.edu.ec>
2025-02-03 18:24:26 -0500

    Actualizacion base de datos

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bonoRoutes = require('./routes/bonoRoutes');
-require('dotenv').config(); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: 'https://frontend-bono.vercel.app', // Reemplaza con la URL de tu frontend desplegado
+  origin: 'https://frontend-bono.vercel.app', 
   optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
 
 app.use(express.json());
 
-const mongoUri = process.env.MONGO_URI;
+// Definir la URI de conexión directamente
+const mongoUri = 'mongodb+srv://jhonicm:1234@clustertest.e64rl.mongodb.net/Prueba_Jc';
 mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error('Error de conexión a MongoDB:', err));
