728c310e2f5ba92272fd6913e89cb62e15214405
Jhon Cordova <jcordovam8@est.ups.edu.ec>
2025-02-03 16:54:32 -0500

    Update app.js to use environment variable for MongoDB connection

--- app.js
+++ app.js
@@ -9,12 +9,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors()); 
 
-mongoose.connect('mongodb+srv://jhonicm:1234@clustertest.e64rl.mongodb.net/Prueba_Jc', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('Conectado a MongoDB'))
-.catch(err => console.error('Error de conexión a MongoDB:', err));
+const mongoUri = process.env.MONGO_URI;
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch(err => console.error('Error de conexión a MongoDB:', err));
 
 app.use('/api/bonos', bonoRoutes);
 
