f59039d00318bb681c48c756c2d1760b1c49d5f2
Jhon Cordova <jcordovam8@est.ups.edu.ec>
2025-02-03 17:38:24 -0500

    Update backend

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bonoRoutes = require('./routes/bonoRoutes');
+require('dotenv').config(); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
